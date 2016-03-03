821325777af46a4d54c322f3f6108202c1b9cca2
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-03-03 11:26:08 +0530

    Updated InstanceExample with complete comments

---
+++ src/main/java/com/shc/vulkan/InstanceExample.java
@@ -0,0 +1,110 @@
+package com.shc.vulkan;
+
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.vulkan.VKCapabilities;
+import org.lwjgl.vulkan.VkApplicationInfo;
+import org.lwjgl.vulkan.VkInstance;
+import org.lwjgl.vulkan.VkInstanceCreateInfo;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.vulkan.KHRSurface.*;
+import static org.lwjgl.vulkan.VK10.*;
+
+/**
+ * This example demonstrates the creation of a Vulkan instance. It shows what are the VkApplicationInfo and the
+ * VkInstanceCreateInfo structs, how to create them, when to free them, and also how to request the extensions.
+ *
+ * @author Sri Harsha Chilakapati
+ */
+public class InstanceExample extends VulkanExample
+{
+    public static void main(String[] args)
+    {
+        title = "Vulkan Instance Example";
+        new InstanceExample().start();
+    }
+
+    private VkInstance instance;
+
+    @Override
+    public VkInstance initVulkan()
+    {
+        // We need to say which extensions to enable at the time of creating the instance.
+        // Create a PointerBuffer with memory enough to hold pointers for all the extension names.
+        PointerBuffer enabledExtensionNames = memAllocPointer(1);
+
+        // Encode the surface extension names into a ByteBuffer so we can put it in the PointerBuffer.
+        // Also note that it is a must to use MALLOC as BufferAllocator and NIO will not work.
+        ByteBuffer KHR_SURFACE_EXTENSION = memEncodeASCII(VK_KHR_SURFACE_EXTENSION_NAME, BufferAllocator.MALLOC);
+
+        // Add the extensions to the PointerBuffer and flip the buffer. In order to present something
+        // we must request the KHR_SURFACE_EXTENSION, without which, the instance will act like an offscreen context.
+        enabledExtensionNames.put(KHR_SURFACE_EXTENSION);
+        enabledExtensionNames.flip();
+
+        // The VkApplicationInfo struct contains information about the application that we are going to create.
+        VkApplicationInfo appInfo = VkApplicationInfo.calloc()
+                .sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
+                .pApplicationName("Vulkan Instance Example")
+                .pEngineName("")
+                .apiVersion(VK_MAKE_VERSION(1, 0, 3));
+
+        // The VkInstanceCreateInfo struct contains information about the Vulkan instance, and refers to the appInfo.
+        VkInstanceCreateInfo instInfo = VkInstanceCreateInfo.calloc()
+                .sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+                .pNext(NULL)
+                .pApplicationInfo(appInfo)
+                .ppEnabledExtensionNames(enabledExtensionNames);
+
+        // The PointerBuffer enough to hold one pointer, the PointerBuffer is not a pointer, but it's contents are.
+        PointerBuffer pInstance = memAllocPointer(1);
+
+        // Create the instance. The instance handle is stored in the PointerBuffer which we can use now.
+        vkCreateInstance(instInfo, null, pInstance);
+
+        // Get the VkInstance handle from the pointer
+        instance = new VkInstance(pInstance.get(), instInfo);
+
+        // Free the pointer buffer, not the VkInstance struct
+        memFree(pInstance);
+
+        // Free the VkApplicationInfo and VkInstanceCreateInfo structs, we no longer need them in our application.
+        appInfo.free();
+        instInfo.free();
+
+        // Free the extension names, we don't need them now.
+        memFree(enabledExtensionNames);
+        memFree(KHR_SURFACE_EXTENSION);
+
+        // Print out the instance capabilities
+        VKCapabilities capabilities = instance.getCapabilities();
+
+        System.out.println("Vulkan10: " + capabilities.Vulkan10);
+        System.out.println("VK_KHR_display: " + capabilities.VK_KHR_display);
+        System.out.println("VK_KHR_surface: " + capabilities.VK_KHR_surface);
+        System.out.println("VK_KHR_swapchain: " + capabilities.VK_KHR_swapchain);
+        System.out.println("VK_EXT_debug_report: " + capabilities.VK_EXT_debug_report);
+        System.out.println("VK_KHR_xlib_surface: " + capabilities.VK_KHR_xlib_surface);
+        System.out.println("VK_KHR_win32_surface: " + capabilities.VK_KHR_win32_surface);
+        System.out.println("VK_KHR_display_swapchain: " + capabilities.VK_KHR_display_swapchain);
+        System.out.println("VK_KHR_sampler_mirror_clamp_to_edge: " + capabilities.VK_KHR_sampler_mirror_clamp_to_edge);
+
+        // Return instance to attach to the display so rendering can be done.
+        return instance;
+    }
+
+    @Override
+    public void render()
+    {
+        // No rendering in this example, this is just to explain how to create the instance.
+    }
+
+    @Override
+    public void cleanUp()
+    {
+        // Destroy the vulkan instance.
+        vkDestroyInstance(instance, null);
+    }
+}
---
+++ src/main/java/com/shc/vulkan/VulkanExample.java
@@ -0,0 +1,113 @@
+package com.shc.vulkan;
+
+import org.lwjgl.vulkan.VkInstance;
+
+import java.nio.LongBuffer;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFWVulkan.*;
+import static org.lwjgl.vulkan.KHRSurface.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * This is the base class that all the Vulkan examples in this repository extends from. This class takes care of the
+ * window creation using GLFW and also the event loop for the window. It also provides a utility method (macro in C)
+ * called as {@code VK_MAKE_VERSION} which allows to create the version.
+ *
+ * @author Sri Harsha Chilakapati
+ */
+public abstract class VulkanExample
+{
+    /**
+     * The ID of the window that the example uses to display the Vulkan output to the user.
+     */
+    protected long windowID;
+
+    /**
+     * The title string of the window. Can be changed to change the title of the window before starting
+     * the event loop.
+     */
+    protected static String title;
+
+    /**
+     * This is a utility method that does the job of the VK_MAKE_VERSION macro in the C sources. This is used to pack
+     * the Vulkan version that these demos require into a single int which we can pass to VkApplicationInfo struct.
+     *
+     * @param major The major Vulkan version.
+     * @param minor The minor Vulkan version.
+     * @param patch The patch Vulkan version.
+     *
+     * @return A single int that represents the packed Vulkan version number.
+     */
+    public static int VK_MAKE_VERSION(int major, int minor, int patch)
+    {
+        return (major << 22) | (minor << 12) | patch;
+    }
+
+    /**
+     * The job of this method is to initialize Vulkan and return the VkInstance struct handle so that it is used to
+     * create a surface and attaches with the window. The initialization code should request the KHR_SURFACE extension
+     * to be able to create the surface.
+     *
+     * @return The VkInstance handle to the successfully initialized Vulkan instance.
+     */
+    public abstract VkInstance initVulkan();
+
+    /**
+     * This method is used to present anything onto the screen. It will be called throughout the life of the example
+     * application.
+     */
+    public abstract void render();
+
+    /**
+     * This method is called before closing the example, and is required to do any cleanup of resources like the vulkan
+     * instance, any buffers that are in use, shaders etc.,
+     */
+    public abstract void cleanUp();
+
+    /**
+     * The main heart of an example, it runs the event loop and manages the creation and destruction of the window and
+     * also creates the surface for Vulkan.
+     */
+    public void start()
+    {
+        glfwInit();
+
+        // Check if Vulkan is available, if not, exit early.
+        if (glfwVulkanSupported() == GLFW_FALSE)
+            throw new UnsupportedOperationException("Vulkan driver is not found on this machine. Please update your drivers.");
+
+        // Create a GLFW window with no API
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+        if (title == null)
+            title = "Vulkan Tests";
+
+        windowID = glfwCreateWindow(800, 600, title, NULL, NULL);
+
+        // Initialize Vulkan and get the instance.
+        VkInstance vulkanInstance = initVulkan();
+
+        // Create the surface and attach to the window.
+        LongBuffer pSurface = memAllocLong(1);
+        int err = glfwCreateWindowSurface(vulkanInstance, windowID, null, pSurface);
+
+        if (err != 0)
+            throw new IllegalStateException("Could not create surface for Vulkan");
+
+        long surfaceID = pSurface.get();
+
+        memFree(pSurface);
+
+        while (glfwWindowShouldClose(windowID) == GLFW_FALSE)
+        {
+            glfwPollEvents();
+            render();
+        }
+
+        vkDestroySurfaceKHR(vulkanInstance, surfaceID, null);
+        cleanUp();
+
+        glfwTerminate();
+    }
+}
